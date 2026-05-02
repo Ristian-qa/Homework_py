a5abf57155125cbb7a729ad217101ec89837be0b
Kristina <kristina.dyachenko0@mail.ru>
2026-05-02 15:42:26 -0700

    Add files via upload

---
+++ lesson2/lesson_2_task_5.py
@@ -0,0 +1,11 @@
+def month_to_season(season):
+    if season == 12 or 1 <= season <= 2:
+        return "Зима"
+    if 3 <= season <= 5:
+        return "Весна"
+    if 6 <= season <= 8:
+        return "Лето"
+    if 9 <= season <=11:
+        return "Осень"
+season = int(input ("Введите номер месяца (1-12): "))
+print (month_to_season(season))
